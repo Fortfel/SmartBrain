48a0d82c62aa476da7e8ee2e9dea91ccf9b550a4
Marcin Dubiel <fortfel@gmail.com>
2025-05-07 23:07:16 +0200

    some work on figuring out tsparticles plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { type Container, type ISourceOptions } from '@tsparticles/engine'
+import { loadPolygonMaskPlugin } from '@tsparticles/plugin-polygon-mask'
+// import { loadAll } from '@tsparticles/all' // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from 'tsparticles' // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from '@tsparticles/slim' // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from '@tsparticles/basic' // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { particlesOptions, options, logo1, logo2 } from '@/assets/particlesOptions.ts'
+
 import { Header } from '@/componenets/Header.tsx'
 import { MainContent } from '@/componenets/MainContent.tsx'
 
 // Configuration
-const BRAIN_ANIMATION_DURATION = 2 // in seconds
+const BRAIN_ANIMATION_DURATION = 1.8 // in seconds
 
 const App = (): React.JSX.Element => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false)
@@ -13,11 +22,39 @@ const App = (): React.JSX.Element => {
       (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
     )
   })
+  const [areParticlesLoaded, setAreParticlesLoaded] = React.useState(false)
+
+  const particlesOptionsWithTheme: ISourceOptions = React.useMemo(() => {
+    return {
+      ...logo1,
+    }
+  }, [isDarkMode])
 
   React.useEffect(() => {
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light')
     document.documentElement.dataset.theme = isDarkMode ? 'dark' : 'light'
   }, [isDarkMode])
+
+  React.useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // You can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+
+      // We could either run loadAll(engine) only or separately loadSlim(engine) and loadPolygonMaskPlugin(engine)
+      // Second option has smaller file size
+
+      // await loadAll(engine)
+      // await loadFull(engine)
+      await loadSlim(engine)
+      // await loadBasic(engine)
+      await loadPolygonMaskPlugin(engine)
+    }).then(() => {
+      setAreParticlesLoaded(true)
+    })
+  }, [])
+
+  const particlesLoaded = async (container?: Container) => {
+    console.log('Particles container loaded:', container)
+  }
 
   function handleThemeChange() {
     setIsDarkMode(!isDarkMode)
@@ -27,6 +64,11 @@ const App = (): React.JSX.Element => {
 
   return (
     <>
+      {areParticlesLoaded && (
+        <>
+          <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={particlesOptionsWithTheme} />
+        </>
+      )}
       {/*<div className="absolute inset-0 -z-30 h-full w-full bg-[linear-gradient(to_right,#80808034_1px,transparent_1px),linear-gradient(to_bottom,#80808034_1px,transparent_1px)]  bg-size-[50px_50px]"></div>*/}
       <div className={`relative flex flex-col gap-15 overflow-x-hidden`}>
         <div className="hero-background"></div>
