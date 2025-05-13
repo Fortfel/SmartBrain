9af1677b8361d0403cbbe0285b54678fc600b637
Marcin Dubiel <fortfel@gmail.com>
2025-05-13 22:49:52 +0200

    used clamp() in navigation instead of media queries

--- src/componenets/Navigation.tsx
+++ src/componenets/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation = ({ isLoggedIn, ...themeControllerProps }: NavigationProps): R
   const loginButton = <a className="cursor-pointer text-lg font-bold">{isLoggedIn ? 'Sign Out' : 'Sign In'}</a>
 
   return (
-    <nav className="line-b flex w-full items-center gap-2 px-2 py-5 text-base-content sm:gap-4 sm:px-5 [&_a:hover]:text-current/70">
+    <nav className="line-b flex w-full items-center gap-[clamp(0.5rem,2.5vw,1.5rem)] px-[clamp(0.5rem,2.5vw,1.5rem)] py-5 text-base-content [&_a:hover]:text-current/70">
       <div className="flex flex-1 cursor-default items-center gap-2">
         <Brain className="size-6 fill-current" />
         <h2 className="text-2xl font-bold">SmartBrain</h2>
