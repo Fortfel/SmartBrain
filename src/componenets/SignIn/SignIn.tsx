be430a045283b31ecdd309fa52ed2553ded0b8e9
Marcin Dubiel <fortfel@gmail.com>
2025-06-04 23:44:33 +0200

    update

--- src/componenets/SignIn/SignIn.tsx
+++ src/componenets/SignIn/SignIn.tsx
@@ -10,23 +10,23 @@ type SignInProps = {
 const SignIn = ({ ref }: SignInProps): React.JSX.Element => {
   const [route, setRoute] = React.useState('login')
 
-  const isLogin = route === 'login'
+  const isLoginRoute = route === 'login'
 
   return (
     <dialog id="my_modal" className="modal" ref={ref}>
       <div className="modal-box sm:p-10">
         <SvgBrain className="hidden size-8 w-full justify-center fill-current sm:flex" />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">
-          Sign {isLogin ? 'In to your account' : 'Up'}
+          Sign {isLoginRoute ? 'In to your account' : 'Up'}
         </h2>
 
         <div className="mt-10">
           <form method="dialog">
             <button className="btn absolute top-2 right-2 btn-circle btn-ghost btn-sm">✕</button>
-            {isLogin ? <Login /> : <Register />}
+            {isLoginRoute ? <Login /> : <Register />}
           </form>
 
-          {isLogin ? (
+          {isLoginRoute ? (
             <p className="mt-5 text-center text-sm/6 text-base-content/50">
               Don't have an account?{' '}
               <button
