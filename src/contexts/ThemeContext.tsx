fd61d98b9bb50487c095c959ff7ef3165a111e94
Marcin Dubiel <fortfel@gmail.com>
2025-06-05 23:06:48 +0200

    update for ThemeContext

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -15,7 +15,7 @@ type ThemeProviderProps = {
   defaultTheme?: ThemeType
 }
 
-const ThemeContext = React.createContext<ThemeContextType | undefined>(undefined)
+const ThemeContext = React.createContext<ThemeContextType | null>(null)
 
 const useTheme = (): ThemeContextType => {
   const context = React.useContext(ThemeContext)
@@ -27,13 +27,18 @@ const useTheme = (): ThemeContextType => {
 
 const ThemeProvider = ({ children, defaultTheme = 'light' }: ThemeProviderProps): React.JSX.Element => {
   const [theme, setTheme] = React.useState<ThemeType>(() => {
+    // Check if the user has a saved theme preference
     const savedTheme = localStorage.getItem('theme') as ThemeType
     if (savedTheme && (savedTheme === 'light' || savedTheme === 'dark')) {
       return savedTheme
     } else {
+      // If not, use the system preference
       return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : defaultTheme
     }
   })
+
+  const isDarkMode = theme === 'dark'
+  const isLightMode = theme === 'light'
 
   React.useEffect(() => {
     localStorage.setItem('theme', theme)
@@ -45,10 +50,19 @@ const ThemeProvider = ({ children, defaultTheme = 'light' }: ThemeProviderProps)
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
   }, [])
 
-  const isDarkMode = theme === 'dark'
-  const isLightMode = theme === 'light'
+  // Memoize the context value to prevent unnecessary re-renders
+  const contextValue = React.useMemo(
+    () => ({
+      theme,
+      setTheme,
+      toggleTheme,
+      isDarkMode,
+      isLightMode,
+    }),
+    [theme, toggleTheme, isDarkMode, isLightMode],
+  )
 
-  return <ThemeContext value={{ theme, setTheme, toggleTheme, isDarkMode, isLightMode }}>{children}</ThemeContext>
+  return <ThemeContext value={contextValue}>{children}</ThemeContext>
 }
 
 export { type ThemeType, type ThemeContextType, type ThemeProviderProps, ThemeContext, useTheme, ThemeProvider }
