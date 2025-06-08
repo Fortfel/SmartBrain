fcb6a05379243d2ca89ee58099ee4fd877d36d8d
Marcin Dubiel <fortfel@gmail.com>
2025-06-08 22:27:50 +0200

    added Register logic

--- src/componenets/SignIn/Register.tsx
+++ src/componenets/SignIn/Register.tsx
@@ -1,31 +1,77 @@
 import * as React from 'react'
+import { type RegisterResponse, useAuth } from '@/contexts/AuthContext.tsx'
 
-const Register = (): React.JSX.Element => {
+type RegisterProps = {
+  onSuccess: () => void
+}
+
+const Register = ({ onSuccess }: RegisterProps): React.JSX.Element => {
+  const [_formState, formAction, isPending] = React.useActionState<RegisterResponse | null, FormData>(
+    handleRegister,
+    null,
+  )
+  const { register } = useAuth()
+
+  async function handleRegister(_prevState: RegisterResponse | null, formData: FormData): Promise<RegisterResponse> {
+    const name = formData.get('name') as string
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const response = await register({ name, email, password })
+    if (response.success) {
+      onSuccess()
+    }
+
+    console.log(response)
+    return response
+  }
+
   return (
-    <fieldset className="fieldset space-y-6">
-      <div>
-        <label className="label text-sm/6 font-medium">Name</label>
-        <input type="name" className="input input-lg mt-2 w-full" placeholder="Name" />
-      </div>
+    <form action={formAction}>
+      <fieldset className="fieldset space-y-1">
+        <div>
+          <label className="label text-sm/6 font-medium">Name</label>
+          <input type="name" name="name" className="validator input input-lg mt-2 w-full" placeholder="Name" required />
+          <p className="validator-hint">Enter your name</p>
+        </div>
 
-      <div>
-        <label className="label text-sm/6 font-medium">Email</label>
-        <input type="email" className="input input-lg mt-2 w-full" placeholder="Email" />
-      </div>
+        <div>
+          <label className="label text-sm/6 font-medium">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="validator input input-lg mt-2 w-full"
+            placeholder="Email"
+            required
+          />
+          <p className="validator-hint">Enter valid email address</p>
+        </div>
 
-      <div>
-        <label className="label text-sm/6 font-medium">Password</label>
-        <input type="password" className="input input-lg mt-2 w-full" placeholder="Password" />
-      </div>
+        <div>
+          <label className="label text-sm/6 font-medium">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="validator input input-lg mt-2 w-full"
+            placeholder="Password"
+            required
+            minLength={5}
+          />
+          <p className="validator-hint">Must be more than 4 characters</p>
+        </div>
 
-      <button
-        className="btn text-sm/6 font-semibold btn-primary"
-        type="submit"
-        onClick={(event) => event.preventDefault()}
-      >
-        Register
-      </button>
-    </fieldset>
+        <button className="btn text-sm/6 font-semibold btn-primary" type="submit" disabled={isPending}>
+          {isPending ? (
+            <>
+              <span className="loading loading-sm loading-spinner"></span>
+              Registering...
+            </>
+          ) : (
+            'Register'
+          )}
+        </button>
+      </fieldset>
+    </form>
   )
 }
 
